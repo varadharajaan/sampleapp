fbfc019ed9598db28a8855ca96d544b554ab2ebd
varadharajan <varad.d@oracle.com>
2022-01-20 20:14:00 +0530

    initial commit

---
+++ src/main/java/in/bushansirgur/springboot/crudapi/model/Employee.java
@@ -0,0 +1,75 @@
+package in.bushansirgur.springboot.crudapi.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_employee")
+public class Employee {
+
+	@Column
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	@Column
+	private String name;
+	@Column
+	private String gender;
+	@Column
+	private String department;
+	@Column
+	private String city;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String dob) {
+		this.city = dob;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", department=" + department + ", dob="
+				+ city + "]";
+	}
+	
+	
+}
